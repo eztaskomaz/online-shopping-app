e3f35f0a8224c6b8068dfe8e5eb5780205d8dfaa
eztaskomaz <eztaskomaz@gmail.com>
2021-03-06 22:31:16 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {commerce} from "./lib/commerce";
 import './App.css';
 
 import {Products, NavBar, Cart} from './components';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 const App = () => {
     const [products, setProducts] = useState([]);
@@ -30,12 +31,19 @@ const App = () => {
     console.log(cart);
 
     return (
-        <div className="App">
-            <h1>Online Shopping App</h1>
-            <NavBar totalItems={cart.total_items}/>
-            {/*<Products products={products} onAddToCart={handleAddToCart}/>*/}
-            <Cart cart={cart}/>
-        </div>
+        <Router>
+            <div className="App">
+                <NavBar totalItems={cart.total_items}/>
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart}/>
+                    </Route>
+                    <Route exact path="/cart">
+                        <Cart cart={cart}/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
